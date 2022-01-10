85365d8e85d2e5da3cb55ef5366b4846be13f8ba
nikolay <expert33@list.ru>
2022-01-10 09:44:48 +0300

    change local

--- local/integration/importFromXml.php
+++ local/integration/importFromXml.php
@@ -5,5 +5,13 @@ pre('start');
 #-------------ТУТ ВАШ КОД
 #Что нужно сделать: Загрузить XML в PHP (рекомендую simplexml_load_file). Далее вывести на экран каждый элемент со свойствами
 #Цель: Аккуратно окунуть Вас в работу с самим PHP и посмотреть у кого возникнут сложности с чистым PHP. Далее мы имея данные в массивах/обьектах научимся загружать это непосредственно в Bitrix.
+echo '<pre>';
+$path = 'data/data.xml';
+if(file_exists($path)) {
+    foreach (json_decode(json_encode(simplexml_load_file($path)), true) as $products) {
+        var_dump($products);
+    }
+}
+echo '</pre>';
 #-------------КОНЕЦ КОДА
 pre('done.');
